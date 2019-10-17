fb20eb49af8c31cd1abb1393745027f1f13b62ec
atsushi oba <m5231104@u-aizu.ac.jp>
2019-10-17 12:01:47 +0900

    fix

--- src/train_bert.py
+++ src/train_bert.py
@@ -112,7 +112,7 @@ def main():
     #
     no_decay = ['bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': args.weight_decay},
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
         {'params': [p for n, p in model.named_parameters() if     any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
     optimizer = optim.Adam(optimizer_grouped_parameters)
